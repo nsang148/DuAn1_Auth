0092febc8b2b4277bd248b369cc96f17b1043ace
Quocanh1306 <109933873+Quocanh1306@users.noreply.github.com>
2022-11-29 14:20:07 +0700

    qa
    sủa hóa đơn

--- DuAn1/src/Repository/HoaDonRepository.java
+++ DuAn1/src/Repository/HoaDonRepository.java
@@ -2,7 +2,10 @@ package Repository;
 
 import DomainModels.HoaDon;
 import DomainModels.HoaDonCT;
+//import DomainModels.HoaDonCT;
 import Untility.DBContext;
+import ViewModels.HoaDonChiTietReponse;
+import ViewModels.HoaDonReponse;
 import java.util.ArrayList;
 import java.util.List;
 import java.sql.*;
@@ -19,29 +22,16 @@ public class HoaDonRepository {
         conn = new DBContext();
     }
 
-    public List<HoaDon> layHoaDon() {
-        List<HoaDon> listHD = new ArrayList<>();
-        String sql = "SELECT * FROM HOADON order by MA";
+    public List<HoaDonReponse> layHoaDon() {
+        List<HoaDonReponse> listHD = new ArrayList<>();
+        String sql = "select NGAYTAO, hd.MA, hdct.SOLUONG * hdct.DONGIA as [Tong tien], nv.TEN, hd.TINHTRANG from HOADON hd join HOADONCHITIET hdct on hd.id = hdct.ID_HOADON join NHANVIEN nv on hd.ID_NHANVIEN = nv.Id";
 
         try {
             Connection con = DBContext.getConnection();
             Statement st = con.createStatement();
             ResultSet rs = st.executeQuery(sql);
             while (rs.next()) {
-                String id = rs.getString("Id");
-                String idKH = rs.getString("ID_KHACHHANG");
-                String idNV = rs.getString("ID_NHANVIEN");
-                String idVi = rs.getString("ID_VIDIEM");
-                String ma = rs.getString("MA");
-                String ngayTao = rs.getString("NGAYTAO");
-                String ngayThanhToan = rs.getString("NGAYTHANHTOAN");
-                String tienKhachDua = rs.getString("TIENKHACHDUA");
-                String tongTien = rs.getString("TONGTIEN");
-                int tt = rs.getInt("TINHTRANG");
-                String diemKH = rs.getString("SODIEMKHACHHANG");
-                HoaDon hd = new HoaDon(id, idKH, idNV, idVi, ma, ngayTao, ngayThanhToan, tienKhachDua, tongTien, diemKH, tt);
-                listHD.add(hd);
-
+                listHD.add(new HoaDonReponse(rs.getString(1), rs.getString(2), rs.getDouble(3), rs.getString(4), rs.getInt(5)));
             }
         } catch (Exception e) {
             System.out.println("Loi khi lay danh sach hoa don");
@@ -49,31 +39,19 @@ public class HoaDonRepository {
         return listHD;
     }
 
-    public List<HoaDonCT> layHoaDonCT(String id) {
-        List<HoaDonCT> listHDCT = new ArrayList<>();
-        String sql = "select HDCT.Id,HDCT.ID_SACH,HDCT.ID_HOADON,HDCT.SOLUONG,HDCT.DONGIA,HDCT.TIENTHUA,HDCT.TIENGIAMGIA,HDCT.TINHTRANG from HOADONCHITIET HDCT join HOADON HD "
-                + "on HDCT.ID_HOADON=HD.Id where HD.Id=?";
-
-        try {
-            Connection conn = DBContext.getConnection();
-            Statement st = conn.createStatement();
-            ResultSet rs = st.executeQuery(sql);
+    public List<HoaDonChiTietReponse> layHoaDonCT(String ma) {
+        List<HoaDonChiTietReponse> list = new ArrayList<>();
+        String select = "select s.TEN, s.Gia, hdct.SOLUONG, hdct.SOLUONG * s.GIA from SACH s join HOADONCHITIET hdct on s.Id = hdct.ID_SACH join HOADON hd on hd.Id = hdct.ID_HOADON where hd.MA = ?";
+        try ( Connection con = DBContext.getConnection();  PreparedStatement sttm = con.prepareStatement(select)) {
+            sttm.setString(1, ma);
+            ResultSet rs = sttm.executeQuery();
             while (rs.next()) {
-                String idHDCT = rs.getString("Id");
-                String idSach = rs.getString("ID_SACH");
-                String idHD = rs.getString("ID_HOADON");
-                int soLuong = rs.getInt("SOLUONG");
-                String donGia = rs.getString("DONGIA");
-                String tienThua = rs.getString("TIENTHUA");
-                String tienGiamGia = rs.getString("TIENGIAMGIA");
-                int tt = rs.getInt("TINHTRANG");
-                HoaDonCT hdct = new HoaDonCT(idHDCT, idSach, idHD, donGia, tienThua, tienGiamGia, soLuong, tt);
-                listHDCT.add(hdct);
-
+                list.add(new HoaDonChiTietReponse(rs.getString(1), rs.getDouble(2), rs.getInt(3), rs.getDouble(4)));
             }
+            return list;
         } catch (Exception e) {
-            System.out.println("Loi khi lay danh sach hoa don ct");
+            e.printStackTrace();
         }
-        return listHDCT;
-    }
+        return null;
+}
 }
