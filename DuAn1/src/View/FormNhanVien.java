1be7767a4ce1f50dc3336b37f2ab0ca39d066783
trancuong-meth <ttrancuongg2003@gmail.com>
2022-11-30 23:24:07 +0700

    cuong

--- DuAn1/src/View/FormNhanVien.java
+++ DuAn1/src/View/FormNhanVien.java
@@ -21,10 +21,12 @@ public class FormNhanVien extends javax.swing.JInternalFrame {
     private String isClicked;
     
     public FormNhanVien() {
+        service = new NhanVienServiceimpl();
         initComponents();
         this.setBorder(javax.swing.BorderFactory.createEmptyBorder(0,0,0,0));
         BasicInternalFrameUI ui = (BasicInternalFrameUI) this.getUI();
         ui.setNorthPane(null); 
+        this.loadTable();
     }
 
     /**
