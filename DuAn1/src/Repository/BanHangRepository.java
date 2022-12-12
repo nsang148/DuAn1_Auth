2c42f225b22b4387690f00a0b9fdb9262adbe42b
Quocanh1306 <109933873+Quocanh1306@users.noreply.github.com>
2022-12-13 01:51:47 +0700

    qa

--- DuAn1/src/Repository/BanHangRepository.java
+++ DuAn1/src/Repository/BanHangRepository.java
@@ -40,6 +40,41 @@ public class BanHangRepository {
         return listHD;
     }
      
+     public List<SanPhamThanhToan> getALLSPCB(String ten) {
+        List<SanPhamThanhToan> listHD = new ArrayList<>();
+        String sql = "select S.TEN, S.Ma ,tl.TEN, tg.TEN, nxb.TEN, s.SOLUONGTON, s.Gia from SACH s join THELOAI tl on s.ID_THELOAI = tl.Id join TACGIA tg on s.ID_TACGIA = tg.Id join NXB nxb on s.ID_NXB = nxb.Id where tl.TEN = ?";
+
+        try ( Connection con = DBContext.getConnection();  PreparedStatement sttm = con.prepareStatement(sql)) {
+            sttm.setString(1, ten);
+            ResultSet rs = sttm.executeQuery();
+            while (rs.next()) {
+                listHD.add( new SanPhamThanhToan(rs.getString(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5), rs.getInt(6), rs.getDouble(7)));
+            }
+            return listHD;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+     
+     public List<SanPhamThanhToan> search(String ten) {
+        List<SanPhamThanhToan> listHD = new ArrayList<>();
+        String sql = "select S.TEN, S.Ma ,tl.TEN, tg.TEN, nxb.TEN, s.SOLUONGTON, s.Gia from SACH s join THELOAI tl on s.ID_THELOAI = tl.Id join TACGIA tg on s.ID_TACGIA = tg.Id join NXB nxb on s.ID_NXB = nxb.Id where s.TEN like N'%"+ten+"%'";
+
+        try ( Connection con = DBContext.getConnection();  PreparedStatement sttm = con.prepareStatement(sql)) {
+            sttm.setString(1, ten);
+            ResultSet rs = sttm.executeQuery();
+            while (rs.next()) {
+                listHD.add( new SanPhamThanhToan(rs.getString(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5), rs.getInt(6), rs.getDouble(7)));
+            }
+            return listHD;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    
+     
      public List<HoaDonThanhToan> getAllHD() {
         List<HoaDonThanhToan> listHD = new ArrayList<>();
         String sql = "select hd.MA, hd.NGAYTAO, nv.TEN, hd.TINHTRANG from HOADON hd join NHANVIEN nv on hd.ID_NHANVIEN = nv.Id order by NGAYTAO desc";
